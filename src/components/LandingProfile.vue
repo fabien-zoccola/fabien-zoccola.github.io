692a12df517607d2be62fb95f157decf51f6bc2d
BioTheWolff <biothewolff@gmx.fr>
2022-02-01 23:31:54 +0100

    simple hello world

---
+++ src/components/LandingProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="landing-profile">
+    <div>Hello, I'm Fabien Zoccola !</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingProfile",
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LandingProfile />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LandingProfile from "@/components/LandingProfile.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    LandingProfile,
   },
 };
 </script>

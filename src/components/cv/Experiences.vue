3e87ab187659936129e4c8da54207cf31ea70988
BioTheWolff <biothewolff@gmx.fr>
2022-02-13 16:29:35 +0100

    adding CV experiences POC to website

---
+++ src/components/cv/Experiences.vue
@@ -0,0 +1,29 @@
+<template>
+  <template v-for="ex in items">
+    <nu-card>
+      <nu-flow>
+        <nu-block :key="ex['type']">{{ ex["type"] }}</nu-block>
+        <nu-block :key="ex['jobTitle']">{{ ex["jobTitle"] }}</nu-block>
+        <nu-block :key="ex['company']">{{ ex["company"] }}</nu-block>
+      </nu-flow>
+    </nu-card>
+  </template>
+</template>
+
+<script>
+export default {
+  name: "Experiences",
+  data: () => ({
+    items: null,
+  }),
+  created() {
+    fetch("/json/experiences.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data;
+      });
+  },
+};
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+
+import Home from "@/views/Home.vue";
+import LandingProfile from "@/components/home/LandingProfile";
+
+import CV from "@/views/CV.vue";
+import Experiences from "@/components/cv/Experiences";
 
 const routes = [
   {
+    // Home page
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    // CV page
+    path: "/cv",
+    name: "CV",
+    component: CV,
+    children: [
+      {
+        // "main" page of the CV
+        // render the default component
+        path: "",
+        component: LandingProfile,
+      },
+      {
+        path: "experiences",
+        component: Experiences,
+      },
+    ],
   },
   // {
   //   path: "/about",

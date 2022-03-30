d4ce169dfdfc9aee6ee83aacb0b39fcac3ab0ca3
BioTheWolff <biothewolff@gmx.fr>
2022-03-30 20:15:20 +0200

    preparing for production compilation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
 
-export { default as ProfilePicture } from "./profile-picture.png"
+export { default as ProfilePicture } from "./profile-picture.jpg"
 export { default as LandingBanner } from "./landing-banner.jpg"
